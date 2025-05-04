6c3dda3f60115a9469c62966bc853e562e38254c
Davmike <davidmikeladze00@gmail.com>
2025-05-04 14:10:01 +0400

    feat: add components on app folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import { ThemeProvider } from './contexts/ThemeContext';
+import { TaskProvider } from './contexts/TaskContext';
+import Header from './components/Header';
+import TaskList from './components/TaskList';
+import TaskForm from './components/TaskForm';
 
 function App() {
-
   return (
-    <>
-      <div>
-      </div>
-    </>
-  )
+    <ThemeProvider>
+      <TaskProvider>
+        <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors duration-300">
+          <Header />
+          <main className="container mx-auto py-6">
+            <TaskList />
+            <TaskForm />
+          </main>
+        </div>
+      </TaskProvider>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
